1b7c9d078709986d8484962e4f0815cb4877c4ec
Mr. GoodGuy <emirramzi@gmail.com>
2025-02-27 20:14:21 +0100

    Deleted comments

--- ConsoleAppBlackjack/Program.cs
+++ ConsoleAppBlackjack/Program.cs
@@ -6,34 +6,6 @@ namespace ConsoleAppBlackjack
     {
         static void Main(string[] args)
         {
-            /*
-            // Skapar en kortlek och blandar den
-            Deck deck = new Deck();
-            deck.Shuffle();
-
-        // Skapat spelare och en dealer via konstruktor 
-        string dittNamn;
-        Console.WriteLine("Vad heter du?");
-
-        dittNamn = Console.ReadLine();
-
-        Player player1 = new Player(dittNamn);
-        Dealer dealerHusetLuffare = new Dealer();
-
-        // Ger spelaren två kort
-        player1.ReceiveCard(deck.DrawCard());
-        player1.ReceiveCard(deck.DrawCard());
-
-        // Ger dealern två kort
-        dealerHusetLuffare.ReceiveCard(deck.DrawCard());
-        dealerHusetLuffare.ReceiveCard(deck.DrawCard());
-
-        // Skriver ut bådas händer
-        Console.WriteLine(player1);
-        Console.WriteLine($"Dealer visar: {dealerHusetLuffare.Hand[0]} och ett dolt kort.");
-
-        Console.ReadKey();
-           */
             Player playerDaniel = new Player("Daniel");
             Card drawforDaniel = new("10", "Diamonds", 10);
 
